d13ce995a14c67da75a0b35188ebae12051330cc
jeseungwook <jsw992@gmail.com>
2025-02-25 18:48:47 +0900

    init commit

---
+++ src/main/java/com/geukrock/geuckrockfrontserver/GeuckrockFrontServerApplication.java
@@ -0,0 +1,13 @@
+package com.geukrock.geuckrockfrontserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeuckrockFrontServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeuckrockFrontServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geukrock/geuckrockfrontserver/GeuckrockFrontServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geukrock.geuckrockfrontserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeuckrockFrontServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
