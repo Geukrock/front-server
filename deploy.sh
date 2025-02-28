f6be2e9f5d168e130ee1c71f9356f1086ace05b4
jeseungwook <jsw992@gmail.com>
2025-02-28 10:54:33 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-APP_NAME=geuckrock-front-server
-REPOSITORY=/home/ubuntu/geuckrock/
 
-echo "> Check the currently running container"
-CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
+# 도커 로그인 환경변수 설정
+export DOCKER_USERNAME=jsw992@gmail.com
+export DOCKER_PASSWORD=sy@1021602
 
-if [ -z "$CONTAINER_ID" ];
-then
-  echo "> No such container is running."
-else
-  echo "> Stop and remove container: $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
+
+# 도커 로그인 및 도커 이미지 다운로드
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+docker pull matt1235/plac:latest
+
+
+# 'plac-server'라는 이름의 도커 컨테이너가 실행 중인 경우, 컨테이너를 중지하고 삭제
+if [ $(docker ps -aq -f name=plac-server) ]; then
+    docker stop plac-server
+    docker rm plac-server
 fi
 
-echo "> Remove previous Docker image"
-docker rmi "$APP_NAME"
 
-echo "> Build Docker image"
-docker build -t "$APP_NAME" "$REPOSITORY"
+# 도커 컨테이너 실행시 필요한 환경변수 설정
+export SERVER_PORT="{생략}"
 
-echo "> Run the Docker container"
-docker run -d -p 3000:8080 --name "$APP_NAME" "$APP_NAME"
+# 도커 컨테이너 실행
+docker run -d \
+  --name plac-server \
+  -p $SERVER_PORT:8080 \
+
+  matt1235/plac:latest
